06f8494825b7d0ade0dcf2b7c11d64e2cc9620c0
curdriceaurora <sra@outlook.com>
2015-10-16 11:21:31 -0700

    new Xcode Project created

---
+++ Alcolator/Alcolator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Alcolator
+//
+//  Created by Rahul Aurora Srinivasan on 10/16/15.
+//  Copyright © 2015 curdriceaurora. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Alcolator/Alcolator/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Alcolator
+//
+//  Created by Rahul Aurora Srinivasan on 10/16/15.
+//  Copyright © 2015 curdriceaurora. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
