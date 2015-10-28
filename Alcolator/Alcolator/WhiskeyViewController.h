7472f5c485f6f86264c14901794e00676bb5dcc2
curdriceaurora <sra@outlook.com>
2015-10-28 16:01:02 -0700

    assignment 20 complete

---
+++ Alcolator/Alcolator/WhiskeyViewController.h
@@ -0,0 +1,13 @@
+//
+//  WhiskeyViewController.h
+//  Alcolator
+//
+//  Created by Rahul Aurora Srinivasan on 10/28/15.
+//  Copyright © 2015 curdriceaurora. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface WhiskeyViewController : ViewController
+
+@end
